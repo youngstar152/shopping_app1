09edb002ac425da4bb929a0916c2a1ef5cdc35d6
tsubasa_nagoya_u <tsubasa.kato.2000@gmail.com>
2022-09-13 20:55:20 +0900

    プロフィールの設定

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from base.models import Item,Category,Tag
+from base.models import Item,Category,Tag,User,Profile
 from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin
+from base.forms import UserCreationForm
 # Register your models here.
 
 class TagInline(admin.TabularInline):
@@ -9,8 +11,33 @@ class TagInline(admin.TabularInline):
 class ItemAdmin(admin.ModelAdmin):
     inlines=[TagInline]
     exclude=['tags']
-    
+ 
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+ 
+ 
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password',)}),
+        (None, {'fields': ('is_active', 'is_admin',)}),
+    )
+ 
+    list_display = ('username', 'email', 'is_active',)
+    list_filter = ()
+    ordering = ()
+    filter_horizontal = ()
+ 
+    add_fieldsets = (
+        (None, {'fields': ('username', 'email', 'is_active',)}),
+    )
+ 
+    add_form = UserCreationForm
+ 
+    inlines = (ProfileInline,)
+        
 admin.site.register(Item,ItemAdmin)
 admin.site.register(Category)
 admin.site.register(Tag)
+admin.site.register(User, CustomUserAdmin)
 admin.site.unregister(Group)
