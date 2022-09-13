e896519869f995770317777421bbd21854d2cb94
tsubasa_nagoya_u <tsubasa.kato.2000@gmail.com>
2022-09-13 14:17:33 +0900

    トップ画面の設定

---
+++ base/views/item_views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from base.models import Item
+
+class IndexListView(ListView):
+    model=Item
+    template_name='pages/index.html'
