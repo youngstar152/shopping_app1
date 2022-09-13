8b974bb8720a17ab473a79b81771d4cafde49995
tsubasa_nagoya_u <tsubasa.kato.2000@gmail.com>
2022-09-13 14:01:17 +0900

    管理画面の設定

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from base.models import Item,Category,Tag
+from django.contrib.auth.models import Group
 # Register your models here.
+
+class TagInline(admin.TabularInline):
+    model=Item.tags.through
+
+class ItemAdmin(admin.ModelAdmin):
+    inlines=[TagInline]
+    exclude=['tags']
+    
+admin.site.register(Item,ItemAdmin)
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.unregister(Group)
