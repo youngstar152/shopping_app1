6505d61ce28a7c5a8df04b381aebfe39d5242fc7
tsubasa_nagoya_u <tsubasa.kato.2000@gmail.com>
2022-09-13 11:43:07 +0900

    secretkey .gitignoreの追加

--- config/settings.py
+++ config/settings.py
@@ -11,21 +11,28 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+env=environ.Env()
+root=environ.Path(BASE_DIR / 'secrets')
 
+#本番環境用
+#env.read_env(root('.env.prod'))
+
+#開発環境用
+env.read_env(root('.env.dev'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1qrjp(zf#imn3nel_zz3ung8roqd62w%egn1unw&4y0oei7(xa'
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
 # Application definition
